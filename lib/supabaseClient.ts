933db302f648fcf315a82e828c0b3bbffa6d39de
borgesr18 <borgesr18@yahoo.com.br>
2025-07-12 15:45:54 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 // Diretório: lib/supabaseClient.ts
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createBrowserClient(
+export const supabase = createClientComponentClient();
+
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
