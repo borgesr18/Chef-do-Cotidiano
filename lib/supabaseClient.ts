86c03be71af9ea9ec70d8fecdff1b653c83d1037
borgesr18 <borgesr18@yahoo.com.br>
2025-07-12 20:29:30 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,7 @@
 // lib/supabaseClient.ts
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
+// Esta função já usa automaticamente as variáveis de ambiente:
+// NEXT_PUBLIC_SUPABASE_URL
+// NEXT_PUBLIC_SUPABASE_ANON_KEY
 export const supabase = createBrowserSupabaseClient();
-
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
