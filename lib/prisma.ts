7c507eefed5cd27d74c9f24333f7b618770e9acc
borgesr18 <borgesr18@yahoo.com.br>
2025-07-12 06:27:54 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,11 @@
+// Arquivo: /lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // Evita múltiplas instâncias em dev
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  });
+  globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
