9fc2a9e415a31bda5a990c4f9485a649b81d64fe
borgesr18 <borgesr18@yahoo.com.br>
2025-07-12 08:05:32 -0300

    Add files via upload

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,3 @@
-// Arquivo: /lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
@@ -6,6 +5,9 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ log: ['query'] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
