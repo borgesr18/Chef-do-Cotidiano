b16f9eb99febfa6c124d6a30e6da63a487a7364e
borgesr18 <borgesr18@yahoo.com.br>
2025-08-10 19:56:21 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
